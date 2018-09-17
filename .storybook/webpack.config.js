650af7699f93edb01921768b9909d0117f1e799f
Samuel Meuli <me@samuelmeuli.com>
2018-09-17 11:26:33 +0200

    Add @storybook/addon-storysource

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,12 +1,32 @@
-const path = require("path");
+const path = require('path');
+
 
 module.exports = {
 	module: {
 		rules: [
 			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: 'style-loader',
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true,
+						},
+					},
+				]
+			},
+			{
+				test: /\.stories\.jsx?$/,
+				loaders: [require.resolve('@storybook/addon-storysource/loader')],
+				enforce: 'pre',
+			},
+			{
 				test: /\.svg$/,
 				loader: 'svg-inline-loader',
-				include: path.resolve(__dirname, "../")
+				include: path.resolve(__dirname, '../')
 			}
 		]
 	}

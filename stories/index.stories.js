9eae609d872f329920c7ce1f5e5ebd675e1671dd
Samuel Meuli <me@samuelmeuli.com>
2018-09-17 11:26:00 +0200

    Add prop stories

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,41 +1,75 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+
+import './style.css';
 import SimpleSvg from '../lib/SimpleSvg';
 import svgImport from './example-import.svg';
 
 
-const size = 100;
-const stroke = 'red';
 const svgString = '<svg id="star" xmlns="http://www.w3.org/2000/svg" width="255" height="240" viewBox="0 0 51 48"><path fill="none" stroke="#000" d="M25 1l6 17h18L35 29l5 17-15-10-15 10 5-17L1 18h18z" /></svg>';
-const svgUrl = 'example-url.svg';
 
 
-storiesOf('SimpleSvg', module)
+storiesOf('Import methods', module)
 	.add('Using import', () => (
 		<SimpleSvg
 			src={svgImport}
 			title="Example 1"
-			height={size}
-			width={size}
-			stroke={stroke}
+			height={100}
+			width={100}
+			stroke="red"
 		/>
 	))
 	.add('From string', () => (
 		<SimpleSvg
 			src={svgString}
 			title="Example 2"
-			height={size}
-			width={size}
-			stroke={stroke}
+			height={100}
+			width={100}
+			stroke="red"
 		/>
 	))
 	.add('From path', () => (
 		<SimpleSvg
-			src={`${svgUrl}#star`}
+			src="./example-url.svg#star"
 			title="Example 3"
-			height={size}
-			width={size}
+			height={100}
+			width={100}
 			fill="none"
-			stroke={stroke}
+			stroke="red"
+		/>
+	));
+
+storiesOf('Props', module)
+	.add('Fill opacity', () => (
+		<SimpleSvg
+			src={svgImport}
+			title="Example 1"
+			height={100}
+			width={100}
+			stroke="red"
+			fill="red"
+			fillOpacity={0.2}
+		/>
+	))
+	.add('Stroke opacity', () => (
+		<SimpleSvg
+			src={svgImport}
+			title="Example 1"
+			height={100}
+			width={100}
+			stroke="red"
+			strokeOpacity={0.2}
+		/>
+	))
+	.add('Custom style', () => (
+		<SimpleSvg
+			src={svgImport}
+			title="Example 1"
+			height={100}
+			width={100}
+			stroke="red"
+			svgStyle={{
+				'stroke-dasharray': 4
+			}}
 		/>
 	));

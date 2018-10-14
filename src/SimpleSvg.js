111920de05b3c5f63704cec33c22a0ee1716b4be
Samuel Meuli <me@samuelmeuli.com>
2018-10-14 13:53:00 +0200

    Convert to PureComponent

--- src/SimpleSvg.js
+++ src/SimpleSvg.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 import { appendStyleDef, replaceChildTag } from './utils';
@@ -34,115 +34,117 @@ const defaultProps = {
 };
 
 
-export default function SimpleSvg(props) {
-	const {
-		src,
-		className,
-		title,
-		description,
-		role,
-		height,
-		width,
-		fill,
-		fillOpacity,
-		stroke,
-		strokeOpacity,
-		svgStyle
-	} = props;
+export default class SimpleSvg extends PureComponent {
+	render() {
+		const {
+			src,
+			className,
+			title,
+			description,
+			role,
+			height,
+			width,
+			fill,
+			fillOpacity,
+			stroke,
+			strokeOpacity,
+			svgStyle
+		} = this.props;
 
-	if (!src) {
-		throw Error('react-simple-svg: Missing "src" prop');
-	}
-
-	// If src is SVG string
-	if (src.startsWith('<svg')) {
-		// Generate random class name and apply styles to class
-		const randClass = `svg-${Math.random().toString().substring(2)}`;
-		let styleDef;
-		if (fill || fillOpacity || stroke || strokeOpacity) {
-			styleDef = `<![CDATA[
-				.${randClass} circle,
-				.${randClass} ellipse,
-				.${randClass} line,
-				.${randClass} path,
-				.${randClass} polygon,
-				.${randClass} polyline,
-				.${randClass} rect {
-					${fill ? `fill: ${fill};` : ''}
-					${fillOpacity ? `fill-opacity: ${fillOpacity};` : ''}
-					${stroke ? `stroke: ${stroke};` : ''}
-					${strokeOpacity ? `stroke-opacity: ${strokeOpacity};` : ''}
-				}
-			]]>`;
+		if (!src) {
+			throw Error('react-simple-svg: Missing "src" prop');
 		}
 
-		// Convert SVG string to HTML node for access to attributes and children
-		const svgWrapper = document.createElement('div');
-		svgWrapper.innerHTML = src;
-		const svg = svgWrapper.firstElementChild;
+		// If src is SVG string
+		if (src.startsWith('<svg')) {
+			// Generate random class name and apply styles to class
+			const randClass = `svg-${Math.random().toString().substring(2)}`;
+			let styleDef;
+			if (fill || fillOpacity || stroke || strokeOpacity) {
+				styleDef = `<![CDATA[
+					.${randClass} circle,
+					.${randClass} ellipse,
+					.${randClass} line,
+					.${randClass} path,
+					.${randClass} polygon,
+					.${randClass} polyline,
+					.${randClass} rect {
+						${fill ? `fill: ${fill};` : ''}
+						${fillOpacity ? `fill-opacity: ${fillOpacity};` : ''}
+						${stroke ? `stroke: ${stroke};` : ''}
+						${strokeOpacity ? `stroke-opacity: ${strokeOpacity};` : ''}
+					}
+				]]>`;
+			}
 
-		// Update SVG child nodes
-		if (title) {
-			replaceChildTag(svg, 'title', title, true);
-		}
-		if (description) {
-			replaceChildTag(svg, 'desc', description);
-		}
-		if (styleDef) {
-			appendStyleDef(svg, styleDef);
+			// Convert SVG string to HTML node for access to attributes and children
+			const svgWrapper = document.createElement('div');
+			svgWrapper.innerHTML = src;
+			const svg = svgWrapper.firstElementChild;
+
+			// Update SVG child nodes
+			if (title) {
+				replaceChildTag(svg, 'title', title, true);
+			}
+			if (description) {
+				replaceChildTag(svg, 'desc', description);
+			}
+			if (styleDef) {
+				appendStyleDef(svg, styleDef);
+			}
+
+			// Update SVG attributes
+			// Class
+			svg.classList.add(randClass);
+			if (className) {
+				svg.classList.add(className);
+			}
+			// Role
+			svg.setAttribute('role', role);
+			// Dimensions
+			svg.style.height = `${height}px`;
+			svg.style.width = `${width}px`;
+			// Styles
+			Object.keys(svgStyle).forEach((key) => {
+				svg.style[key] = `${svgStyle[key]}`;
+			});
+
+			return (
+				<span
+					className="svg-wrapper"
+					dangerouslySetInnerHTML={{
+						__html: svgWrapper.innerHTML
+					}}
+				/>
+			);
 		}
 
-		// Update SVG attributes
-		// Class
-		svg.classList.add(randClass);
-		if (className) {
-			svg.classList.add(className);
-		}
-		// Role
-		svg.setAttribute('role', role);
-		// Dimensions
-		svg.style.height = `${height}px`;
-		svg.style.width = `${width}px`;
-		// Styles
-		Object.keys(svgStyle).forEach((key) => {
-			svg.style[key] = `${svgStyle[key]}`;
-		});
-
+		// If src is URL
 		return (
-			<span
-				className="svg-wrapper"
-				dangerouslySetInnerHTML={{
-					__html: svgWrapper.innerHTML
+			<svg
+				className={className}
+				height={height}
+				width={width}
+				role={role}
+				style={{
+					...svgStyle,
+					fill,
+					fillOpacity,
+					stroke,
+					strokeOpacity
 				}}
-			/>
+			>
+				{ title && <title>{title}</title>}
+				{ description && <desc>{description}</desc>}
+				<use
+					xlinkHref={src} // SVG v1.1 (still required by Safari)
+					href={src} // SVG v2
+					height="100%"
+					width="100%"
+				/>
+			</svg>
 		);
 	}
-
-	// If src is URL
-	return (
-		<svg
-			className={className}
-			height={height}
-			width={width}
-			role={role}
-			style={{
-				...svgStyle,
-				fill,
-				fillOpacity,
-				stroke,
-				strokeOpacity
-			}}
-		>
-			{ title && <title>{title}</title>}
-			{ description && <desc>{description}</desc>}
-			<use
-				xlinkHref={src} // SVG v1.1 (still required by Safari)
-				href={src} // SVG v2
-				height="100%"
-				width="100%"
-			/>
-		</svg>
-	);
 }
 
 SimpleSvg.propTypes = propTypes;
